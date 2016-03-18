21d75db868b6456b5881ddb26086c44abf6b9caa
Ryker Blunck <rykerblunck@gmail.com>
2016-03-18 14:03:20 -0600

    starts and stops audio

--- app/js/main.js
+++ app/js/main.js
@@ -1,18 +1,21 @@
 'use strict';
 
 var audioCtx = new window.AudioContext();
-var oscillator = audioCtx.createOscillator();
-oscillator.type = 'sine';
-oscillator.frequency.value = 3000;
-// oscillator.start();
-oscillator.connect(audioCtx.destination);
 
 const body = document.querySelector('body');
-body.onkeydown = (e) => {
-  console.log(e.keyCode);
+var currentNode;
 
+body.onkeydown = (e) => {
+  console.log(e.keyCode, 1);
+  var oscillator = audioCtx.createOscillator();
+  oscillator.type = 'sine';
+  oscillator.frequency.value = 3000;
+  oscillator.start();
+  oscillator.connect(audioCtx.destination);
+  currentNode = oscillator;
 };
 
-body.onKeyUp = (e) => {
-  console.log(e.keyCode);
+body.onkeyup = (e) => {
+  console.log(e.keyCode, 2);
+  currentNode.disconnect();
 };
